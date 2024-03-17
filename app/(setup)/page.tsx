2ec7ad487c4b1f89a3089628ca7ddabd7b85b136
div-dev <divyanshchawla12@gmail.com>
2024-03-18 01:33:48 +0530

    image upload fuctionality and api call

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
     const profile = await initialProfile();
@@ -17,7 +18,7 @@ const SetupPage = async () => {
     if (server){
         return redirect(`/servers/${server.id}`);
     }
-    return <div>Create Server</div>;
+    return <InitialModal />;
 }
  
 export default SetupPage;   
--- lib/db.ts
+++ lib/db.ts
@@ -4,8 +4,7 @@ declare global {
   var prisma: PrismaClient | undefined;
 };
 
-const db = globalThis.prisma || new PrismaClient();
+export const db = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
 
-export {db};
